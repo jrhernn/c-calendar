5b5e40214c490bcfbc143aa56a5df02fe4f145ac
jrhernn <jrhernn@proton.me>
2023-03-03 11:39:34 -0600

    Added loops to print months and days of the week

--- src/main.c
+++ src/main.c
@@ -3,5 +3,20 @@
 
 int main()
 {
+    int year, daysPerMonth;
+    printf("\nEnter the year:");
+    scanf("%d", &year);
+
     char *months[] = {"January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    int days_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    if (year % 4 != 0)
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            daysPerMonth = days_month[i];
+            printf("\n%s", months[i]);
+            printf("\n   Sun   Mon   Tues   Wed  Thurs   Fri   Sat\n");
+        }
+    }
 }
