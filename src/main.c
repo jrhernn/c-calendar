dfef50d1d0ad3d5b3881db4b2667a066a95e0fc2
jrhernn <jrhernn@proton.me>
2023-03-03 10:56:11 -0600

    first commit

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char *months[] = {"January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+}
